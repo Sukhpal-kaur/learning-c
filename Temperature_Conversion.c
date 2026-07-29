59d32fc0d2dda20b2a795e018461a3a5325f7d2d
Sukhpal kaur <sukhpalkaur063@gmail.com>
2026-07-29 13:18:12 +0530

    Create Temperature_Conversion.c

---
+++ Temperature_Conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float celsius, fahrenheit;
+
+    celsius = 25;
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Temperature in Fahrenheit = %.2f", fahrenheit);
+
+    return 0;
+}
