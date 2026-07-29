b11f6692adfef8747c152dd5ffeaef566a5c6756
Sukhpal kaur <sukhpalkaur063@gmail.com>
2026-07-29 13:03:08 +0530

    Create Area_of_Circle.c

---
+++ Area_of_Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, area;
+
+    radius = 7;
+
+    area = 3.14 * radius * radius;
+
+    printf("Area of Circle = %.2f", area);
+
+    return 0;
+}
