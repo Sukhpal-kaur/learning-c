315cc2261add676724e01f4516d392157ad8cbd6
Sukhpal kaur <sukhpalkaur063@gmail.com>
2026-07-29 15:31:58 +0530

    Create Largest_of_two_numbers.c

---
+++ Largest_of_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 25, b = 40;
+
+    if (a > b)
+    {
+        printf("%d is the largest number.", a);
+    }
+    else
+    {
+        printf("%d is the largest number.", b);
+    }
+
+    return 0;
+}
